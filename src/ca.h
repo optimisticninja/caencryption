6a1e7e7742911de70cd56c2e7852d0fc878e3444
John Holly <j@optimistic.ninja>
2022-01-27 14:02:04 -0700

    Initial commit. (Decryption broken)

---
+++ src/ca.h
@@ -0,0 +1,116 @@
+#pragma once
+
+#include <bitset>
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
+typedef enum { BOUNDARY_PERIODIC, BOUNDARY_ZERO } boundary_t;
+
+template<size_t StateSize> class CA
+{
+  private:
+    bitset<StateSize> _state;
+    vector<bitset<3>> permutations;
+    boundary_t boundary;
+
+    /**
+     * Recursive function to generate all permutations of specified neighborhood cells. Used
+     * for finding the index of a partition within the exhaustive set and using it as a
+     * bit-shift within qualifying rules.
+     * @param permutations: vector to store generated permutations in
+     * @param neighborhood: starting neighborhood to permute
+     * @param cell: cell to operate on
+     */
+    void generate_partition_states(vector<bitset<3>>& permutations, bitset<3>& neighborhood, size_t cell)
+    {
+        if (cell == 3) {
+            permutations.push_back(neighborhood);
+            return;
+        }
+
+        neighborhood[cell] = 0;
+        generate_partition_states(permutations, neighborhood, cell + 1);
+        neighborhood[cell] = 1;
+        generate_partition_states(permutations, neighborhood, cell + 1);
+    }
+
+  public:
+    CA(const bitset<StateSize>& state, boundary_t boundary = BOUNDARY_PERIODIC)
+        : _state(state), boundary(boundary)
+    {
+        bitset<3> neighborhood(3);
+        generate_partition_states(permutations, neighborhood, 0);
+    }
+
+    /**
+     * Run CA for 1 epoch
+     * @param rule CA transition rule to execute
+     */
+    bitset<StateSize> step(unsigned rule)
+    {
+        bitset<StateSize> new_state(_state);
+
+        if (boundary == BOUNDARY_PERIODIC) {
+            for (unsigned cell = 0; cell < _state.size(); cell++) {
+                bitset<3> neighborhood;
+
+                // Create a neighborhood with periodic boundary
+                if (cell == 0) {
+                    neighborhood[0] = _state[_state.size() - 1];
+                    neighborhood[1] = _state[cell];
+                    neighborhood[2] = _state[cell + 1];
+                } else if (cell == (_state.size() - 1)) {
+                    neighborhood[0] = _state[cell - 1];
+                    neighborhood[1] = _state[cell];
+                    neighborhood[2] = _state[0];
+                } else {
+                    neighborhood[0] = _state[cell - 1];
+                    neighborhood[1] = _state[cell];
+                    neighborhood[2] = _state[cell + 1];
+                }
+
+                // get bit offset of partition in this->_gateway_key.partition_permutations
+                unsigned i = 0;
+                for (; i < permutations.size(); i++)
+                    if (permutations[i] == neighborhood)
+                        break;
+                new_state[cell] = (rule >> i) & 1;
+            }
+        } else if (boundary == BOUNDARY_ZERO) {
+            for (unsigned cell = 0; cell < _state.size(); cell++) {
+                bitset<3> neighborhood;
+
+                // Create a neighborhood with null boundary
+                if (cell == 0) {
+                    neighborhood[0] = 0;
+                    neighborhood[1] = _state[cell];
+                    neighborhood[2] = _state[cell + 1];
+                } else if (cell == (_state.size() - 1)) {
+                    neighborhood[0] = _state[cell - 1];
+                    neighborhood[1] = _state[cell];
+                    neighborhood[2] = 0;
+                } else {
+                    neighborhood[0] = _state[cell - 1];
+                    neighborhood[1] = _state[cell];
+                    neighborhood[2] = _state[cell + 1];
+                }
+
+                // get bit offset of neighborhood in permutations
+                unsigned i = 0;
+                for (; i < permutations.size(); i++)
+                    if (permutations[i] == neighborhood)
+                        break;
+                new_state[cell] = (rule >> i) & 1;
+            }
+        } else {
+            assert(true && "Unsupported boundary type");
+        }
+
+        this->_state = new_state;
+        return new_state;
+    }
+
+    bitset<StateSize> state() { return _state; }
+};
---
+++ src/main.cc
@@ -0,0 +1,141 @@
+#include <bitset>
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+#include <set>
+#include <vector>
+
+#include "ca.h"
+#include "concat.h"
+
+using namespace std;
+
+void schedule_key(bitset<64>& round_key, const string& key, uint8_t round)
+{
+    // Create 80 bits of key material from key
+    bitset<80> whole;
+    for (unsigned i = 0; i < key.length(); ++i) {
+        char c = key[i];
+        for (int j = 7; j >= 0 && c; --j) {
+            if (c & 0x1)
+                whole.set(8 * i + j);
+            c >>= 1;
+        }
+    }
+
+    // Divide key into 20 bit sections
+    vector<bitset<20>> A(4);
+    for (unsigned i = 0; i < 4; i++)
+        for (unsigned j = 0; j < 20; j++)
+            A[i][j] = whole[(i * 20) + j];
+
+    for (unsigned i = 0; i < A.size(); i++) {
+        CA<20> ca(A[i]);
+
+        if ((i + 1) % 2 == 0) {
+            A[i] = ca.step(102);
+        } else {
+            A[i] = ca.step(60);
+        }
+    }
+
+    bitset<80> merged = concat<20>(A[0], A[1], A[2], A[3]);
+
+    bool even = round % 2 == 0;
+    unsigned limit = even ? whole.size() : 64;
+    unsigned start = even ? 16 : 0;
+
+    for (unsigned round_bit = 0; start < limit; start++, round_bit++)
+        round_key[round_bit] = merged[start];
+}
+
+uint64_t encrypt(uint64_t plaintext, const char* key)
+{
+    // Divide plaintext into 4 16 bit sections
+    vector<bitset<16>> segments(4);
+    for (size_t i = 0; i < segments.size(); i++) {
+        segments[i] = plaintext >> (64 - (16 * (i + 1)));
+    }
+
+    // Diffusion of plaintext for 4 rounds
+    for (unsigned i = 0; i < 4; i++) {
+        for (unsigned segment = 0; segment < segments.size(); segment++) {
+            CA<16> ca(segments[segment]);
+
+            if ((segment + 1) % 2 == 0) {
+                ca.step(51);
+            } else {
+                ca.step(153);
+            }
+
+            segments[segment] = ca.state();
+        }
+    }
+
+    bitset<64> evolved_plaintext = concat<16>(segments[0], segments[1], segments[2], segments[3]);
+
+    for (unsigned round = 1; round <= 15; round++) {
+        bitset<64> scheduled_key;
+        schedule_key(scheduled_key, key, round);
+        evolved_plaintext ^= scheduled_key;
+
+        CA<evolved_plaintext.size()> ca_reversible(evolved_plaintext);
+        // Confusion of XORd result with reversible rules
+        ca_reversible.step(170);
+        ca_reversible.step(204);
+        ca_reversible.step(15);
+        evolved_plaintext = ca_reversible.state();
+    }
+
+    return evolved_plaintext.to_ulong();
+}
+
+uint64_t decrypt(uint64_t encrypted, const char* key)
+{
+    bitset<64> encrypted_bits(encrypted);
+
+    for (int round = 15; round >= 1; round--) {
+
+        bitset<64> scheduled_key;
+        schedule_key(scheduled_key, key, round);
+        encrypted_bits ^= scheduled_key;
+
+        CA<encrypted_bits.size()> ca(encrypted_bits);
+        ca.step(240);
+        ca.step(204);
+        ca.step(80);
+        encrypted_bits = ca.state();
+    }
+
+    // Divide plaintext into 16 4 bit segments
+    vector<bitset<4>> segments(16);
+
+    for (unsigned i = 0; i < segments.size(); i++)
+        for (size_t j = 0; j < segments[0].size(); j++)
+            segments[i][j] = encrypted_bits[(i * 4) + j];
+
+    for (unsigned i = 1; i <= 4; i++) {
+        for (unsigned segment = 0; segment <= segments.size(); segment++) {
+            CA<4> ca(segments[segment], BOUNDARY_ZERO);
+            segments[segment] = ((segment + 1) % 2 == 0) ? ca.step(51) : ca.step(153);
+        }
+    }
+
+    return concat4(segments[0], segments[1], segments[2], segments[3], segments[4], segments[5], segments[6],
+                   segments[7], segments[8], segments[9], segments[10], segments[11], segments[12],
+                   segments[13], segments[14], segments[15])
+        .to_ulong();
+}
+
+int main()
+{
+    const uint64_t plaintext = 0xDEADBEEFCAFEBABE;
+    // TODO: Derive 80 bit digest from key string
+    const char* key = "CAFEBABEEE";
+
+    cout << "P: " << hex << plaintext << endl;
+    bitset<64> encrypted = encrypt(plaintext, key);
+    cout << "E: " << hex << encrypted.to_ulong() << endl;
+    bitset<64> decrypted = decrypt(encrypted.to_ulong(), key);
+    cout << "D: " << hex << decrypted.to_ulong() << endl;
+}
